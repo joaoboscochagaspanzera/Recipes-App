bd3c0ec212f1071ba6133b0dc7c9448fd8d9ead6
Bernardo Eichstaedt Steinmacher <bsteinmacher@outlook.com>
2023-05-08 17:24:16 -0300

    feat: requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,20 @@
 function Login() {
-  return (<h1>Login</h1>);
+  return (
+    <>
+      <h1>Login</h1>
+      <input
+        data-testid="email-input"
+      />
+      <input
+        data-testid="password-input"
+      />
+      <button
+        data-testid="login-submit-btn"
+      >
+        ENTER
+      </button>
+    </>
+  );
 }
 
 export { Login };
