06786246c8cf7e42fb4b7e8ac1813be9498ab3ac
Bernardo Eichstaedt Steinmacher <bsteinmacher@outlook.com>
2023-05-08 19:17:38 -0300

    fix: lint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,49 +3,54 @@ import { useForm } from '../hooks/useForm';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 
 function Login() {
-  const MIN_CHARACTERS = 6;
-  const REGEX_EMAIL = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const inputEmail = useForm();
   const inputPassword = useForm();
-  const [storagedUser, setStoragedUser] = useLocalStorage('user');
 
-  const handleLogin = useCallback(() => {
+  const MIN_CHARACTERS = 6;
+  const REGEX_EMAIL = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const isValid = REGEX_EMAIL.test(inputEmail.value)
+
+  && (inputPassword.value).length > MIN_CHARACTERS;
+  const [setStoragedUser] = useLocalStorage('user');
+
+  const handleChange = useCallback(() => {
     setStoragedUser({
       email: inputEmail.value,
       password: inputPassword.value,
     });
   }, [inputEmail.value, inputPassword.value, setStoragedUser]);
 
-  const isValid = REGEX_EMAIL.test(inputEmail.value)
-  && (inputPassword.value).length > MIN_CHARACTERS;
   return (
     <>
       <h1>Login</h1>
       <label htmlFor="email">
-        Email
         <input
           data-testid="email-input"
           type="email"
-          onChange={ handleLogin }
+          placeholder="Email"
+          onChange={ handleChange }
           { ...inputEmail }
         />
       </label>
       <label htmlFor="email">
-        Password
         <input
           data-testid="password-input"
           type="password"
-          onChange={ handleLogin }
+          placeholder="Password"
+          onChange={ handleChange }
           { ...inputPassword }
         />
       </label>
-      <button
-        data-testid="login-submit-btn"
-        disabled={ !isValid }
-        onClick={ handleLogin }
-      >
-        ENTER
-      </button>
+      <label htmlFor="submit">
+        <button
+          data-testid="login-submit-btn"
+          type="submit"
+          disabled={ !isValid }
+          onClick={ handleChange }
+        >
+          ENTER
+        </button>
+      </label>
     </>
   );
 }
