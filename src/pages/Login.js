200484505a49d0bcc07e89e327b737f681235f4a
Bernardo Eichstaedt Steinmacher <bsteinmacher@outlook.com>
2023-05-08 17:25:22 -0300

    feat: requisito 3

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,9 +4,11 @@ function Login() {
       <h1>Login</h1>
       <input
         data-testid="email-input"
+        type="email"
       />
       <input
         data-testid="password-input"
+        type="password"
       />
       <button
         data-testid="login-submit-btn"
