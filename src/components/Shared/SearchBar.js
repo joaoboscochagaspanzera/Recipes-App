157b6d23be48e0dce79cdf73d34cfa26c05c550b
MatheusTrein <matheusptrein@hotmail.com>
2023-05-15 17:12:18 -0300

    style: search bar

--- src/components/Shared/SearchBar.js
+++ src/components/Shared/SearchBar.js
@@ -1,9 +1,34 @@
+import '../../styles/SearchBar.css';
+
 function SearchBar() {
   return (
-    <>
-      <h1>SearchBar</h1>
-      <input type="text" data-testid="search-input" />
-    </>
+    <div className="search-bar">
+      <input
+        type="text"
+        data-testid="search-input"
+        placeholder="Search"
+        className="search-input"
+      />
+      <div className="search-bar-interface">
+        <div className="search-bar-filters">
+          <label>
+            <input type="radio" />
+            Ingredient
+          </label>
+          <label>
+            <input type="radio" />
+            Name
+          </label>
+          <label>
+            <input type="radio" />
+            First Letter
+          </label>
+        </div>
+        <button>
+          Search
+        </button>
+      </div>
+    </div>
   );
 }
 
