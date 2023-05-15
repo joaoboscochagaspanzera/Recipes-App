fd45993dae7c4bdb1f897e0f8434d84925b53b3e
Bernardo Eichstaedt Steinmacher <bsteinmacher@outlook.com>
2023-05-15 03:55:47 -0300

    feat: tela receitas prontas

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,15 +1,34 @@
+import { useCallback, useEffect, useState } from 'react';
 import { Header } from '../components/Shared/Header';
-import { MyRecipesFeed } from '../components/MyRecipes/MyRecipesFeed';
+import { MyRecipesFeedDone } from '../components/MyRecipes/MyRecipesFeedDone';
 import { MyRecipesFilterBar } from '../components/MyRecipes/MyRecipesFilterBar';
 import { useRecipes } from '../hooks/useRecipes';
 
 function DoneRecipes() {
   const { doneRecipes } = useRecipes();
+  const [
+    filteredDoneRecipes,
+    setFilteredDoneRecipes,
+  ] = useState(doneRecipes);
+  const [filterSelected, setFilterSelected] = useState('all');
+
+  const handleFilterDoneRecipes = useCallback((type) => {
+    const filteredRecipes = doneRecipes
+      .filter((doneRecipe) => doneRecipe.type === type);
+    setFilteredDoneRecipes(type === 'all' ? doneRecipes : filteredRecipes);
+    if (type !== filterSelected) {
+      setFilterSelected(type);
+    }
+  }, [doneRecipes, filterSelected]);
+
+  useEffect(() => {
+    handleFilterDoneRecipes(filterSelected);
+  }, [doneRecipes, filterSelected, handleFilterDoneRecipes]);
   return (
     <>
       <Header />
-      <MyRecipesFilterBar />
-      <MyRecipesFeed recipes={ doneRecipes } />
+      <MyRecipesFilterBar handleFilterRecipes={ handleFilterDoneRecipes } />
+      <MyRecipesFeedDone recipes={ filteredDoneRecipes } />
       <h1>DoneRecipes</h1>
     </>
   );
